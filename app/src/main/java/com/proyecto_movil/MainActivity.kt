d871efc887103bef8f074c839f55370a99883df9
AlejandroRiveros <alejandroriso@unisabana.edu.co>
2025-10-02 10:09:19 -0500

    implementacion de barra inferior, creacion de activity_login.xml e iconos del menu inferior

--- app/src/main/java/com/proyecto_movil/MainActivity.kt
+++ app/src/main/java/com/proyecto_movil/MainActivity.kt
@@ -1,21 +1,44 @@
 package com.proyecto_movil
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-            //Prueba 1
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+
+        // Listener de clicks en la barra inferior
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    // Ya estás en Home, no hace falta abrir otra Activity
+                    true
+                }
+                /*
+                R.id.nav_courses -> {
+                    startActivity(Intent(this, CoursesActivity::class.java))
+                    true
+                }
+                R.id.nav_calendar -> {
+                    startActivity(Intent(this, CalendarActivity::class.java))
+                    true
+                }
+                R.id.nav_notifications -> {
+                    startActivity(Intent(this, NotificationsActivity::class.java))
+                    true
+                }
+               R.id.nav_profile -> {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                    true
+                }*/
+                else -> false
+            }
         }
     }
 }
+
